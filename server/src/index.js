04186153059a3d00a5155e056d924a5c072661ff
Huy Ha <gnt1078@autuni.ac.nz>
2020-09-09 10:57:37 +1200

    [f] As a guest, I would like to visit the app and register/login with my email - Backend & Frontend - Bootstrap layout is pending - Email confirmation is pending
    [https://trello.com/c/09Nl3GtX]

--- server/src/index.js
+++ server/src/index.js
@@ -1,40 +1,53 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-const path = require('path');
+const express = require("express")
+const path = require("path")
 const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const passport = require("passport")
 const MONGODB_URL = process.env.MONGODB_URL || "mongodb://mongo:27017/mernapp_dev"
 
+const users = require("./routes/api/users")
+
+const app = express()
+
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+)
+app.use(bodyParser.json())
+
+// // DB Config
+// const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
 mongoose
   .connect(MONGODB_URL, { useNewUrlParser: true, useCreateIndex: true }) // Adding new mongo url parser
-  .then(() => {
-    // Constants
-    const PORT = process.env.PORT || 8080;
-    const HOST = '0.0.0.0';
-
-    const CLIENT_BUILD_PATH = path.join(__dirname, '../../client/build');
-
-    // App
-    const app = express();
-
-    // Static files
-    app.use(express.static(CLIENT_BUILD_PATH));
-
-    // API
-    app.get('/api', (req, res) => {
-      res.set('Content-Type', 'application/json');
-      let data = {
-        message: 'Hello world, SDM Team 8 is coming!!!!'
-      };
-      res.send(JSON.stringify(data, null, 2));
-    });
-
-    // All remaining requests return the React app, so it can handle routing.
-    app.get('*', function(request, response) {
-      response.sendFile(path.join(CLIENT_BUILD_PATH, 'index.html'));
-    });
-
-    app.listen(PORT, HOST);
-    console.log(`Running on http://${HOST}:${PORT}`);
-  })
+  .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize())
+
+// Passport config
+require("./config/passport")(passport)
+
+// Routes
+app.use("/api/users", users);
+
+const PORT = process.env.PORT || 8080
+const HOST = '0.0.0.0'
+
+const CLIENT_BUILD_PATH = path.join(__dirname, '../../client/build')
+
+app.use(express.static(CLIENT_BUILD_PATH))
+
+
+app.get('*', function(request, response) {
+  response.sendFile(path.join(CLIENT_BUILD_PATH, 'index.html'))
+})
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`)
