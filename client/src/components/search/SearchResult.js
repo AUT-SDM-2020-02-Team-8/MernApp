a37146bd682d2c94bbd7b420ba98d07aaeaad1a4
Huy Ha <gnt1078@autuni.ac.nz>
2020-10-04 22:45:24 +1300

    Allow user to sort by column

--- client/src/components/search/SearchResult.js
+++ client/src/components/search/SearchResult.js
@@ -9,13 +9,34 @@ class SearchResult extends Component {
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>Title</TableCell>
-              <TableCell>Author</TableCell>
-              <TableCell>Year</TableCell>
-              <TableCell>Type</TableCell>
-              <TableCell>Journal</TableCell>
-              <TableCell>Publisher</TableCell>
-              <TableCell>SE Practice</TableCell>
+              <TableCell onClick={() => this.props.sortHandler('title')}>
+                Title&nbsp;&nbsp;
+                {this.props.sortByColumn === 'title' ? (this.props.direction < 0 ? <i className={'arrow-up'} /> : <i className={'arrow-down'} />) : ''}
+              </TableCell>
+              <TableCell onClick={() => this.props.sortHandler('author')}>
+                Author&nbsp;&nbsp;
+                {this.props.sortByColumn === 'author' ? (this.props.direction < 0 ? <i className={'arrow-up'} /> : <i className={'arrow-down'} />) : ''}
+              </TableCell>
+              <TableCell onClick={() => this.props.sortHandler('year')}>
+                Year&nbsp;&nbsp;
+                {this.props.sortByColumn === 'year' ? (this.props.direction < 0 ? <i className={'arrow-up'} /> : <i className={'arrow-down'} />) : ''}
+              </TableCell>
+              <TableCell onClick={() => this.props.sortHandler('recordType')}>
+                Type&nbsp;&nbsp;
+                {this.props.sortByColumn === 'recordType' ? (this.props.direction < 0 ? <i className={'arrow-up'} /> : <i className={'arrow-down'} />) : ''}
+              </TableCell>
+              <TableCell onClick={() => this.props.sortHandler('journal')}>
+                Journal&nbsp;&nbsp;
+                {this.props.sortByColumn === 'journal' ? (this.props.direction < 0 ? <i className={'arrow-up'} /> : <i className={'arrow-down'} />) : ''}
+              </TableCell>
+              <TableCell onClick={() => this.props.sortHandler('publisher')}>
+                Publisher&nbsp;&nbsp;
+                {this.props.sortByColumn === 'publisher' ? (this.props.direction < 0 ? <i className={'arrow-up'} /> : <i className={'arrow-down'} />) : ''}
+              </TableCell>
+              <TableCell onClick={() => this.props.sortHandler('sePractice')}>
+                SE Practice&nbsp;&nbsp;
+                {this.props.sortByColumn === 'sePractice' ? (this.props.direction < 0 ? <i className={'arrow-up'} /> : <i className={'arrow-down'} />) : ''}
+              </TableCell>
               <TableCell>Claims</TableCell>
             </TableRow>
           </TableHead>
